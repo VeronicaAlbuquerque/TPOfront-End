1a0001bf3d4f60ab1651495b401aec0dc7d50351
VeronicaAlbuquerque <valbuquerquedosreis@gmail.com>
2022-05-27 23:37:38 -0300

    TPO Entrega

---
+++ front/formulario.js
@@ -0,0 +1,11 @@
+//validar nombre
+const btnValidar = document.getElementsByClassName("btn-form")
+btnValidar.addEventListener("click",validarNombre)  
+
+function validarNombre(){
+    const nombreForm= document.getElementById("fname")
+    if(nombreForm ==0){
+        
+    }
+}
+
