669648f56d11b6b1e9e886eb3794a35b18bafd83
VeronicaAlbuquerque <valbuquerquedosreis@gmail.com>
2022-06-26 20:48:53 -0300

    Nueva Entrega TPO

--- front/formulario.js
+++ front/formulario.js
@@ -1,11 +1,101 @@
 //validar nombre
-const btnValidar = document.getElementsByClassName("btn-form")
-btnValidar.addEventListener("click",validarNombre)  
+window.addEventListener("load", function(){
+    let formulario= document.querySelector('form.form-padre');
 
-function validarNombre(){
-    const nombreForm= document.getElementById("fname")
-    if(nombreForm ==0){
+    formulario.addEventListener('submit', function(event){
+        let errores= [];
+
+       
+        let caracteresInvalidos= ["0","1","2","3","4","5","6","7","8","9"];
+        let caracteresInvalidosLetras= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
         
-    }
-}
+        //Nombre
+        let campoNombre = document.getElementById('fname');
+        caracteresInvalidos.forEach(caracter=>{
+            if(campoNombre.value.includes(caracter) == true){
+                errores.push('El campo nombre no debe contener números')
+            }
+
+        })
+        if(campoNombre.value == ""){
+            errores.push('El campo nombre no puede estar vacío')
+        }else if(campoNombre.value.length < 3 || campoNombre.value.length > 20){
+            errores.push('El campo nombre no pude tener menos de 3 caracteres o más de 20 caracteres')
+        }else{
+            console.log(campoNombre.value)
+        };
+
+        //apellido
+        let campoApellido= document.getElementById('lname');
+        caracteresInvalidos.forEach(caracter=>{
+            if(campoApellido.value.includes(caracter) == true){
+                errores.push('El campo apellido no debe contener números')
+            }
+
+        })
+        if(campoApellido.value == ""){
+            errores.push('El campo Apellido no puede estar vacío')
+        }else if( campoApellido.value.length < 3 || campoApellido.value.length > 20){
+            errores.push('El campo Apellido no pude tener menos de 3 caracteres o más de 20 caracteres')
+        }else{
+            console.log(campoApellido.value)
+        };
+
+        //mail
+        let campoEmail= document.getElementById('e-name')
+        if(campoEmail.value == ""){
+            errores.push('El campo E-mail no puede estar vacío')
+        }else if(campoEmail.value.includes("@") == false){
+            errores.push('Ingrese un mail con caracter válido. Por ej: minombre@codoacodo.com')
+        }else{
+            console.log(campoEmail.value)
+        };
+
+        //celular
+        let campoCelular= document.getElementById('cel-name');
+        caracteresInvalidosLetras.forEach(caracteres=>{
+            if(campoCelular.value.includes(caracteres) == true){
+                errores.push('El campo del celular no debe tener letras, sólo números')
+            }
+        })
+        if(campoCelular.value == ""){
+            errores.push('El campo del celular no debe estar vacío')
+        }else if(campoCelular.value != 10){
+            errores.push('el campo del celular debe tener 10 dígitos. Por ej: código de área + 8 números')
+        }else{
+            console.log(campoCelular.value)
+        };
+        
+        //radio button
+        let campoEnvio= document.getElementsByClassName('envio-radio')
+        if(campoEnvio.checked == false){
+            errores.push('Debe seleccionar el envío')
+        }else{
+            console.log(campoEnvio.value)
+        };
+
+        //select
+        let campoZona= document.getElementById('zona');
+        if(campoZona.value == "seleccion" ){
+            errores.push('Debe seleccionar la zona de envío')
+        }else{
+            console.log(campoZona.value)
+        }
+        //si los hay errores no se enviará el formulario
+        if(errores.length > 0 ){
+            event.preventDefault();
+        
+            let listaErrores= document.querySelector('div.errores');
+            for(let i = 0; i<errores.length; i++){
+                listaErrores.innerHTML+= "<li>" + errores[i] + "</li>"
+            }
+        
+        }else{
+            alert('El envío del formulario se realizó con exito');
+
+        }
+      })
+
+})
+
 
--- front/index.js
+++ front/index.js
@@ -1,41 +1,42 @@
 //Funcion mostrar tortas
 const listaTortas=[
-    {imagen : "/img/imgTortas/brownieOreo.jpg",nombre: "Brownie Charlie",precio: 3500},
-    {imagen: "/img/imgTortas/cheeseCake.jpeg",nombre: "Cheesecake",precio: 2800},
-    {imagen: "/img/imgTortas/chocolateAlmendras.jpeg",nombre: "ChocoBlack",precio: 3600},
-    {imagen: "/img/imgTortas/chocotortaEExtreme.jpeg",nombre: "Chocotorta Extreme",precio: 3500},
-    {imagen: "/img/imgTortas/lenon2.jpeg",nombre: "Lemon Pie",precio: 2700},
-    {imagen: "/img/imgTortas/oreo.jpeg",nombre: "Triple Oreo",precio: 3700},
-    {imagen: "/img/imgTortas/SweetOrange.jpeg",nombre: "Sweet Orange",precio: 3100    },
-    {imagen: "/img/imgTortas/tiramisu.jpeg",nombre: "Tiramisu",precio: 2900},
-    {imagen: "/img/imgTortas/brownieDelBos.jpeg",nombre: "Brownie Del Bosque",precio: 3500},
+    {imagen : "../img/imgTortas/brownieOreo.jpg",nombre: "Brownie Charlie",precio: 3500},
+    {imagen: "../img/imgTortas/cheeseCake.jpeg",nombre: "Cheesecake",precio: 2800},
+    {imagen: "../img/imgTortas/chocolateAlmendras.jpeg",nombre: "ChocoBlack",precio: 3600},
+    {imagen: "../img/imgTortas/chocotortaEExtreme.jpeg",nombre: "Chocotorta Extreme",precio: 3500},
+    {imagen: "../img/imgTortas/lenon2.jpeg",nombre: "Lemon Pie",precio: 2700},
+    {imagen: "../img/imgTortas/oreo.jpeg",nombre: "Triple Oreo",precio: 3700},
+    {imagen: "../img/imgTortas/SweetOrange.jpeg",nombre: "Sweet Orange",precio: 3100    },
+    {imagen: "../img/imgTortas/tiramisu.jpeg",nombre: "Tiramisu",precio: 2900},
+    {imagen: "../img/imgTortas/brownieDelBos.jpeg",nombre: "Brownie Del Bosque",precio: 3500},
     
 ];
-const tortasBtn= document.getElementById("variedad-torta");//llamo al div que tendra el evento
-
+const tortasBtn= document.querySelector("#variedad-torta");//llamo al div que tendra el evento
 tortasBtn.addEventListener("click", mostrarTortas);
 
 
-
 function mostrarTortas () {
-
-    let divContenedor= document.getElementById("fila");
-
-    listaTortas.forEach((tortas)=>{
+       let divContenedor= document.getElementById("contenedor-1");
+                
+    listaTortas.forEach((torta) =>{
         
-        const crearDiv= document.createElement("div")
+
+        const crearDiv= document.createElement('div')
         crearDiv.setAttribute("class","visible")
-        crearDiv.innerHTML=
-        `<img class= "img-grid" src=${tortas.imagen} alt="variedad">
-        <h2 class="titulo-grid">${tortas.nombre}</h2>
-        <h3 class="precio-grid">${tortas.precio}</h3>
+        crearDiv.innerHTML =
+        `
+        <img class= "img-grid" src=${torta.imagen} alt="variedad">
+         <h2 class="titulo-grid">${torta.nombre}</h2>
+         <h3 class="precio-grid">$${torta.precio}</h3>
         `
 
         divContenedor.appendChild(crearDiv)
     })
 
     tortasBtn.removeEventListener("click", mostrarTortas)
-}
+};
 
 
 
+
+ 
